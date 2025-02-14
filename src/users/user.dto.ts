04102ba423ad33dcfbac6acc89ecc0384f44b53d
Shekar B <boggavarpucs@outlook.com>
2025-02-13 20:14:09 -0800

    added user dto, class-validator, class-transformer package

---
+++ src/users/user.dto.ts
@@ -0,0 +1,16 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    Length,
+} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    name: string;
+    @IsEmail()
+    email: string;
+    @IsString()
+    @Length(8, 20)
+    password: string;
+}
