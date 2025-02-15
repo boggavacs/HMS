525c7e4ce0119ae42830746e55892378a3c6b19f
Shekar B <boggavarpucs@outlook.com>
2025-02-14 16:17:56 -0800

    convert healthcheck and Application methods in to generics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
+import { ApplicationInfo } from './appModel';
 
 @Module({
   controllers: [AppController],
@@ -9,5 +10,6 @@ import { UsersModule } from 'src/users/users.module';
     ConfigModule.forRoot(), 
     UsersModule
   ],
+  providers: [ApplicationInfo],
 })
 export class AppModule {}
