6f86d0055e3cb98a5e7dfef5eeed2e13b2cfcfde
Shekar B <boggavarpucs@outlook.com>
2025-02-18 21:39:51 -0800

    user service updated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersReposiroty } from './users.repository';
+import { UserModel } from './user.model';
 
 @Module({
   controllers: [UsersController],
