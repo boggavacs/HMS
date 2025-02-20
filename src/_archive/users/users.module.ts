62613f7f136bd329c44dc5df1fc77f04ea8809f7
Prasanna <prasu@Prasannas-MacBook-Pro.local>
2025-02-19 21:12:49 -0800

    add sqlite db & TypeORM for application

--- src/_archive/users/users.module.ts
+++ src/_archive/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import User from './user.entity';
+import { UsersReposiroty } from './users.repository';
+import { UserModel } from './user.model';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-    User
-  ]) ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [
+    UsersService, 
+    UsersReposiroty
+  ]
 })
 export class UsersModule {}
