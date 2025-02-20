5129d8c13eb565d18a3257e051631260d649a57f
Prasanna <prasu@Prasannas-MacBook-Pro.local>
2025-02-19 21:12:49 -0800

    add sqlite db & TypeORM for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from 'src/users/users.module';
 import { ApplicationInfo } from './appModel';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from '../users/user.entity';
 
 @Module({
   controllers: [AppController],
   imports: [
     ConfigModule.forRoot(), 
-    UsersModule
+    TypeOrmModule.forRoot(
+      {
+        type: 'sqlite',
+        database: 'db.sqlite',
+        entities: [User],
+        synchronize: true,
+      }
+    ),
   ],
   providers: [ApplicationInfo],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,4 @@
-import { Controller, Get, Post, Body, Param, Inject, HttpException, HttpStatus } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './user.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-    constructor(public userService: UsersService,
-    ) {}
-
-    @Get()
-    listUsers() {
-        try {
-            return this.userService.getAllUsers();
-        } catch (error) {
-            throw new HttpException('Error fetching users', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @Post('/create')
-    createUser(@Body() body: CreateUserDto) {
-        const user = body;
-        this.userService.createUser<CreateUserDto>(user);
-        // return {
-        //     id: 1,
-        //     name: body.name,
-        //     email: body.email,
-        //     password: body.password,
-        // }
-    }
-
-    @Get('/:id')
-    getUserById(@Param('id') id: string) {
-        return{
-            id: id
-        }
-    }
-}
+export class UsersController {}
