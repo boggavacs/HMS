1683dd0917e2ce80f2f190fb97f15310543e9b8d
Shekar B <boggavarpucs@outlook.com>
2025-02-20 19:37:45 -0800

    add reports entity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ApplicationInfo } from './appModel';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import User from '../users/user.entity';
+import { Reports } from 'src/reports/reports.entity';
 
 @Module({
   controllers: [AppController],
@@ -12,8 +13,8 @@ import User from '../users/user.entity';
     TypeOrmModule.forRoot(
       {
         type: 'sqlite',
-        database: 'db.sqlite',
-        entities: [User],
+        database: './db.sqlite',
+        entities: [User, Reports],
         synchronize: true,
       }
     ),
